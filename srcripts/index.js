a63b2f467d0303ea02efa9f1dd55e05bae83541f
rubi1054 <rubirose1054@gmail.com>
2023-02-24 16:24:50 +0100

    console.log in js

---
+++ srcripts/index.js
@@ -0,0 +1,23 @@
+console.log("Hello World!!!");
+
+//alert("Hello Java Script!!!");
+
+// однострочный комментарий
+
+/*
+многострочный комент
+и еще
+и еще
+*/
+
+let name = "Yana";
+let year = 2023;
+let month = "Feb";
+let a = 10, b = 20, c = 30;
+
+console.log(name);
+console.log(year);
+console.log(month, year);
+console.log("Month is", month, "and year is", year);
+
+console.log("Sum a and b and c is", a+b+c);
