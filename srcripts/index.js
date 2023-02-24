f484b8e57efc79ea2de1b84e837531b096e4f310
rubi1054 <rubirose1054@gmail.com>
2023-02-24 23:04:02 +0100

    переменные присваивание

--- srcripts/index.js
+++ srcripts/index.js
@@ -10,14 +10,40 @@ console.log("Hello World!!!");
 и еще
 */
 
-let name = "Yana";
-let year = 2023;
-let month = "Feb";
-let a = 10, b = 20, c = 30;
+// объявление переменных
+let name = "Yana"; // string
+let year = 2023; // number
+let month = "Feb"; // string
+let a = 10, b = 20, c = 30; // number
+let pi = 3.141596; // number
+let user1 = "Vasya Testovich", user2 = 'Masha Fox', user3 = 'Tom Adson'; // string
+let isMale= false; //boolean
+let isWeekend = true; // boolen
 
+// вывод переменных
 console.log(name);
 console.log(year);
 console.log(month, year);
 console.log("Month is", month, "and year is", year);
 
 console.log("Sum a and b and c is", a+b+c);
+
+//работа с переменными
+let x  = 100;
+x = 1;
+x = 0; 
+x = 9;
+console.log(x);
+x = 'hello';
+console.log(x);
+x = 200;
+console.log(x);
+x = "Tom";
+x = "John Smith";
+console.log(x);
+
+let y = 5, y2 = 20;
+x = y;
+console.log("x=", x, "y=", y);
+x = y2;
+console.log("x=", x, "y=", y);
